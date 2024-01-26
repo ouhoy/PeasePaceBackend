701aca8aef98928eff88c49c8de6358b42ea98dd
abdessamiesairh <asayarh80@gmail.com>
2024-01-26 04:22:50 +0100

    add products

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -9,15 +9,29 @@ CREATE TABLE IF NOT EXISTS Product
 );
 
 INSERT INTO Product (id, title, price, category, origin, image)
-VALUES (1, 'Heirloom tomato', 5.00, 'vegetables', 'San Juan Capistrano, CA', 'tomatto.png');
+VALUES (1, 'Heirloom tomato', 5.00, 'vegetables', 'Souss-Massa, Morocco', 'tomatto.png');
 
 INSERT INTO Product (id, title, price, category, origin, image)
-VALUES (2, 'Sweet Onion', 2.99, 'vegetables', 'San Juan Capistrano, CA', 'sweet-onion.png');
+VALUES (2, 'Sweet Onion', 2.99, 'vegetables', 'Meknes, Morocco', 'sweet-onion.png');
 
 
 INSERT INTO Product (id, title, price, category, origin, image)
-VALUES (3, 'Cherries ', 1.30, 'fruits', 'San Juan Capistrano, CA', 'cherries.png');
+VALUES (3, 'Cherries ', 1.30, 'fruits', 'Sefrou, Morocco', 'cherries.png');
 
 INSERT INTO Product (id, title, price, category, origin, image)
-VALUES (4, 'Avocado ', 8.50, 'fruits', 'San Juan Capistrano, CA', 'Avocado.png');
+VALUES (4, 'Avocado ', 8.50, 'fruits', 'Tancitaro-Michoan, Mexico', 'Avocado.png');
+
+INSERT INTO Product (id, title, price, category, origin, image)
+VALUES (5, 'Red pepper ', 2.00, 'vegetables', 'Tadla-Azilal, Morocco', 'Red pepper.png');
+
+INSERT INTO Product (id, title, price, category, origin, image)
+VALUES (6, 'eggplant ', 3.00, 'vegetables', 'Fes, Morocco', 'eggplant.png');
+
+INSERT INTO Product (id, title, price, category, origin, image)
+VALUES (7, 'Potato ', 2.00, 'vegetables', 'The Atlantic Coast of Casablanca, Morocco', 'Potato.png');
+
+INSERT INTO Product (id, title, price, category, origin, image)
+VALUES (7, 'Carrot ', 5.00, 'vegetables', 'El-Hoceima, Morocco', 'Carrot.png');
+
+
 
