a5ff00b0da6642ea7bb1dff36f2149bad12f7028
abdessamiesairh <asayarh80@gmail.com>
2024-01-26 03:30:14 +0100

    add Cherries product

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -13,3 +13,8 @@ VALUES (1, 'Heirloom tomato', 5.00, 'vegetables', 'San Juan Capistrano, CA', 'to
 
 INSERT INTO Product (id, title, price, category, origin, image)
 VALUES (2, 'Sweet Onion', 2.99, 'vegetables', 'San Juan Capistrano, CA', 'sweet-onion.png');
+
+
+INSERT INTO Product (id, title, price, category, origin, image)
+VALUES (2, 'Cherries ', 1.59, 'fruits', 'San Juan Capistrano, CA', 'cherries.png');
+
